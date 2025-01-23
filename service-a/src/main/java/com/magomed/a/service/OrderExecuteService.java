ae248c042e425ac24d17a376f96ddba7e8e0c08c
dred22 <jhabitey@gmail.com>
2025-01-23 16:44:41 +0100

    AsynApi with basic configuration added

--- service-a/src/main/java/com/magomed/a/service/OrderExecuteService.java
+++ service-a/src/main/java/com/magomed/a/service/OrderExecuteService.java
@@ -1,6 +1,9 @@
 package com.magomed.a.service;
 
 import com.magomed.events.OrderEvent;
+import io.github.springwolf.bindings.kafka.annotations.KafkaAsyncOperationBinding;
+import io.github.springwolf.core.asyncapi.annotations.AsyncOperation;
+import io.github.springwolf.core.asyncapi.annotations.AsyncPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -16,6 +19,17 @@ public class OrderExecuteService {
 
     public void createOrder(String article) {
         var orderEvent = new OrderEvent(article, "CREATED");
+        sendEvent(orderEvent);
+    }
+
+    @AsyncPublisher(
+            operation = @AsyncOperation(
+                    channelName = "${spring.kafka.producer.order-events}",
+                    description = "This producer sends the order event in order to confirm command execution"
+            )
+    )
+    @KafkaAsyncOperationBinding
+    private void sendEvent(OrderEvent orderEvent) {
         kafkaTemplate.send(topic, orderEvent);
     }
 }
--- service-b/src/main/java/com/magomed/b/service/OrderService.java
+++ service-b/src/main/java/com/magomed/b/service/OrderService.java
@@ -1,6 +1,9 @@
 package com.magomed.b.service;
 
 import com.magomed.events.OrderCmd;
+import io.github.springwolf.bindings.kafka.annotations.KafkaAsyncOperationBinding;
+import io.github.springwolf.core.asyncapi.annotations.AsyncOperation;
+import io.github.springwolf.core.asyncapi.annotations.AsyncPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -16,6 +19,17 @@ public class OrderService {
 
     public void createWithName(String name) {
         OrderCmd orderCmd = new OrderCmd("CREATE", name, 1);
+        sendCmd(orderCmd);
+    }
+
+    @AsyncPublisher(
+            operation = @AsyncOperation(
+                    channelName = "${spring.kafka.producer.order-cmd}",
+                    description = "This producer sends the order command in order to manage (create, delete) orders"
+            )
+    )
+    @KafkaAsyncOperationBinding
+    private void sendCmd(OrderCmd orderCmd) {
         kafkaTemplate.send(topic, orderCmd);
     }
 }
