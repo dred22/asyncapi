b411da4fb2a6fb8de160b240ba4c044b523a103c
dred22 <jhabitey@gmail.com>
2025-09-03 14:11:35 +0200

    disable Jdbc for IT

--- service-a/src/test/java/com/magomed/a/AppATests.java
+++ service-a/src/test/java/com/magomed/a/AppATests.java
@@ -1,9 +1,12 @@
 package com.magomed.a;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration(exclude = { HibernateJpaAutoConfiguration.class })
 class AppATests {
 
 	@Test
